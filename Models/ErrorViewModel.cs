b883153c7ccd3764990eabbdb7d31b906312a85a
pawan-ni008 <git008@nimble.com.np>
2025-10-11 15:11:45 +0545

    Initial commit for MyStudio project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
